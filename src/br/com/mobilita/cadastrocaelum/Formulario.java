0782cbc95bc714c990cbda5b352932344a95f4b6
Riposati <guriposa@gmail.com>
2014-11-12 15:55:02 -0200

    DAOs para desacoplamento do java com o SQLite

--- src/br/com/mobilita/cadastrocaelum/Formulario.java
+++ src/br/com/mobilita/cadastrocaelum/Formulario.java
@@ -1,9 +1,25 @@
 package br.com.mobilita.cadastrocaelum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import br.com.mobilita.cadastro.modelo.Aluno;
+import br.com.mobilita.dao.AlunoDAO;
 
 public class Formulario extends ActionBarActivity {
+
+    private formularioHelper helper;
+    private Button bt_gravar;
+    private EditText et_nome;
+    private EditText et_site;
+    private EditText et_endereco;
+    private EditText et_telefone;
+    private RatingBar rb_nota;
+    private Aluno aluno;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -12,6 +28,56 @@ public class Formulario extends ActionBarActivity {
 
         setContentView(R.layout.formulario);
 
-    }
+        Formulario.this.helper = new formularioHelper(Formulario.this);
 
+        /*
+         *
+         * essas duas chamadas servem para organizar a classe Formulário
+         */
+
+        this.et_nome = (EditText) findViewById(R.id.et_nome);
+        this.et_site = (EditText) findViewById(R.id.et_site);
+        this.et_endereco = (EditText) findViewById(R.id.et_endereco);
+        this.et_telefone = (EditText) findViewById(R.id.et_telefone);
+        this.rb_nota = (RatingBar) findViewById(R.id.rb_nota);
+
+        this.bt_gravar = (Button) findViewById(R.id.bt_gravar);
+
+
+        final Intent it = getIntent();
+
+        final Aluno alunoSelecionado = (Aluno) it.getSerializableExtra("alunoSelecionado");
+
+        if (alunoSelecionado != null) {
+            this.bt_gravar.setText("Alterar");
+            this.helper.colocaAlunoParaSerAlterado(alunoSelecionado);
+            // this.et_nome.setText(alunoSelecionado.getNome());
+            // this.et_endereco.setText(alunoSelecionado.getEndereco());
+            // this.et_site.setText(alunoSelecionado.getSite());
+            // this.et_telefone.setText(alunoSelecionado.getTelefone());
+            // this.rb_nota.setRating(alunoSelecionado.getNotas().floatValue());
+        }
+
+        this.bt_gravar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+
+                Formulario.this.aluno = new Aluno();
+
+                Formulario.this.aluno = Formulario.this.helper.pegaAlunoFormulario();
+
+                // Toast.makeText(Formulario.this, Formulario.this.aluno.getNome(),
+                // Toast.LENGTH_LONG)
+                // .show();
+                final AlunoDAO dao = new AlunoDAO(Formulario.this);
+                dao.salva(Formulario.this.aluno);
+                dao.close(); // sempre usar para fechar a conexão com a base de dados!
+                // Toast.makeText(Formulario.this, "Aluno salvo!", Toast.LENGTH_LONG).show();
+                finish(); // / faz o papel do botao voltar
+            }
+        });
+
+
+    }
 }
--- src/br/com/mobilita/cadastrocaelum/ListaAlunos.java
+++ src/br/com/mobilita/cadastrocaelum/ListaAlunos.java
@@ -1,47 +1,59 @@
 package br.com.mobilita.cadastrocaelum;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import br.com.mobilita.cadastro.modelo.Aluno;
+import br.com.mobilita.dao.AlunoDAO;
 
 
 public class ListaAlunos extends ActionBarActivity {
+
+    private ListView lista;
+    private Aluno aluno;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listagemalunos);
 
-        final ListView lista = (ListView) findViewById(R.id.lista);
-        final String nomes[] = {"Gustavo", "Andre", "Fabiana"};
-        final int layout = android.R.layout.simple_list_item_1;
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout, nomes);
+        this.lista = (ListView) findViewById(R.id.lista);
 
-        lista.setAdapter(adapter);
+        registerForContextMenu(this.lista); // para mostrar no menu quando se é dado clique longo
 
         /*
          * 
          * Clique curto usa-se este método
          */
-        lista.setOnItemClickListener(new OnItemClickListener() {
+        this.lista.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(final AdapterView<?> adapter, final View view,
-                                    final int position, final long id) {
+                            final int position, final long id) {
 
-                Toast.makeText(ListaAlunos.this, "Posição clicada = " + (id + 1) + "",
-                    Toast.LENGTH_SHORT).show();
+                // Toast.makeText(ListaAlunos.this, "Posição clicada = " + (id + 1) + "",
+                // Toast.LENGTH_SHORT).show();
 
+                final Aluno alunoClicado = (Aluno) adapter.getItemAtPosition(position);
+                final Intent irPraTelaFormulario = new Intent(ListaAlunos.this, Formulario.class);
+
+                irPraTelaFormulario.putExtra("alunoSelecionado", alunoClicado);
+
+                startActivity(irPraTelaFormulario);
             }
         });
 
@@ -50,19 +62,82 @@ public class ListaAlunos extends ActionBarActivity {
          */
 
 
-        lista.setOnItemLongClickListener(new OnItemLongClickListener() {
+        this.lista.setOnItemLongClickListener(new OnItemLongClickListener() {
 
             @Override
             public boolean onItemLongClick(final AdapterView<?> adapter, final View view,
-                                           final int posicao, final long id) {
+                            final int posicao, final long id) {
+                //
+                // Toast.makeText(ListaAlunos.this,
+                // "Clique longo em = " + adapter.getItemAtPosition(posicao),
+                // Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(ListaAlunos.this,
-                    "Clique longo em = " + adapter.getItemAtPosition(posicao),
-                    Toast.LENGTH_SHORT).show();
-                return true;
+                ListaAlunos.this.aluno = (Aluno) adapter.getItemAtPosition(posicao);
+
+                return false;
+
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu, final View v,
+                                    final ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add("SMS");
+        menu.add("Navegar no site");
+        menu.add("Ligar");
+        final MenuItem deletar = menu.add("Deletar"); // ao clicar longo uma opção do menu pode
+        // responder ao clique através de u evento
+        deletar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(final MenuItem item) {
+
+                final AlunoDAO aluno = new AlunoDAO(ListaAlunos.this);
+
+                aluno.deletar(ListaAlunos.this.aluno);
+
+                // Toast.makeText(ListaAlunos.this, ListaAlunos.this.aluno.getNome(),
+                // Toast.LENGTH_SHORT).show();
+
+                aluno.close(); // sempre ao usar o DAO lembrar de fechar
+
+                carregaLista();
+
+                return false;
             }
         });
 
+        menu.add("Ver no mapa");
+        menu.add("Enviar e-mail");
+    }
+
+    @Override
+    protected void onResume() {
+
+        carregaLista();
+    }
+
+    private void carregaLista() {
+        super.onResume();
+
+        final AlunoDAO alunoDao = new AlunoDAO(this);
+
+        final List<Aluno> alunos = alunoDao.getLista();
+
+        alunoDao.close();
+
+        final int layout = android.R.layout.simple_list_item_1;
+
+        final ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, layout, alunos);
+
+        this.lista.setAdapter(adapter);
     }
 
     @Override
